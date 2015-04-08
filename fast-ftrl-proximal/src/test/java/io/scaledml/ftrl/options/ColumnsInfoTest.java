69e312475412bc1e4a83998cd369407fefe8827c
Ilya Smagin <ilya-sm@yandex-team.ru>
2015-04-08 21:43:02 +0300

    ColumnsMask introd

---
+++ fast-ftrl-proximal/src/test/java/io/scaledml/ftrl/options/ColumnsInfoTest.java
@@ -0,0 +1,50 @@
+package io.scaledml.ftrl.options;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ColumnsInfoTest {
+
+       @Test
+    public void parsesLongMask() {
+        ColumnsMask ilcnn = new ColumnsMask("ilcnn");
+        assertEquals(ColumnsMask.ColumnType.ID, ilcnn.getCategory(0));
+        assertEquals(ColumnsMask.ColumnType.LABEL, ilcnn.getCategory(1));
+        assertEquals(ColumnsMask.ColumnType.CATEGORICAL, ilcnn.getCategory(2));
+        assertEquals(ColumnsMask.ColumnType.NUMERICAL, ilcnn.getCategory(3));
+        assertEquals(ColumnsMask.ColumnType.NUMERICAL, ilcnn.getCategory(4));
+    }
+
+    @Test
+    public void parsesWithBrackets1() {
+        ColumnsMask ilcnn = new ColumnsMask("ic[1]n");
+        assertEquals(ColumnsMask.ColumnType.ID, ilcnn.getCategory(0));
+        assertEquals(ColumnsMask.ColumnType.CATEGORICAL, ilcnn.getCategory(1));
+        assertEquals(ColumnsMask.ColumnType.NUMERICAL, ilcnn.getCategory(2));
+    }
+
+    @Test
+    public void parsesWithBrackets2() {
+        ColumnsMask ilcnn = new ColumnsMask("ic[2]n");
+        assertEquals(ColumnsMask.ColumnType.ID, ilcnn.getCategory(0));
+        assertEquals(ColumnsMask.ColumnType.CATEGORICAL, ilcnn.getCategory(1));
+        assertEquals(ColumnsMask.ColumnType.CATEGORICAL, ilcnn.getCategory(2));
+        assertEquals(ColumnsMask.ColumnType.NUMERICAL, ilcnn.getCategory(3));
+    }
+
+    @Test
+    public void parsesWithBracketsAndTheRest() {
+        ColumnsMask ilcnn = new ColumnsMask("lc[2]n[4]c");
+        assertEquals(ColumnsMask.ColumnType.LABEL, ilcnn.getCategory(0));
+        assertEquals(ColumnsMask.ColumnType.CATEGORICAL, ilcnn.getCategory(1));
+        assertEquals(ColumnsMask.ColumnType.CATEGORICAL, ilcnn.getCategory(2));
+        assertEquals(ColumnsMask.ColumnType.NUMERICAL, ilcnn.getCategory(3));
+        assertEquals(ColumnsMask.ColumnType.NUMERICAL, ilcnn.getCategory(4));
+        assertEquals(ColumnsMask.ColumnType.NUMERICAL, ilcnn.getCategory(5));
+        assertEquals(ColumnsMask.ColumnType.NUMERICAL, ilcnn.getCategory(6));
+        assertEquals(ColumnsMask.ColumnType.CATEGORICAL, ilcnn.getCategory(7));
+        assertEquals(ColumnsMask.ColumnType.CATEGORICAL, ilcnn.getCategory(100));
+    }
+
+}
